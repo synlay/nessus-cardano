86161f583a02e7829e1364bc90bccaa2d976987f
David Robakowski <david.robakowski@synlay.com>
2022-07-27 22:50:32 +0200

    Fix libsodium build

--- nix/libsodium/default.nix
+++ nix/libsodium/default.nix
@@ -14,7 +14,8 @@ pkgs.stdenv.mkDerivation {
 
   src = builtins.fetchGit {
     url = "https://github.com/input-output-hk/libsodium";
-    rev = "66f017f1";
+    allRefs = true;
+    rev = "66f017f16633f2060db25e17c170c2afa0f2a8a1";
   };
 
   buildInputs = [
